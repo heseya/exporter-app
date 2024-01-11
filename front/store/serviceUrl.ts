a490b27f64e54d48f548559b550cd29e04e054f8
Artur Mędrygał <artur.medrygal@escolasoft.com>
2024-01-11 12:33:32 +0100

    typo fix

--- front/store/serviceUrl.ts
+++ front/store/serviceUrl.ts
@@ -3,7 +3,7 @@ import { computed, reactive } from 'vue'
 const state = reactive({ serviceUrl: import.meta.env.VITE_SERVICE_API_URL as string })
 
 const setServiceUrl = (url: string) => {
-  console.log('Updated Core API URL to:', url)
+  console.log('Updated Service URL to:', url)
   state.serviceUrl = url
 }
 
