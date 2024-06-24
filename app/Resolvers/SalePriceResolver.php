298c420d7618df0534ce98a76ad4c1355c7734a8
Witold Wiśniewski <witold.michal.wisniewski@gmail.com>
2024-06-24 11:17:07 +0200

    Ignore sale price

--- app/Resolvers/SalePriceResolver.php
+++ app/Resolvers/SalePriceResolver.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 namespace App\Resolvers;
 
 use App\Models\Field;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class SalePriceResolver implements LocalResolver
 {
     public static function resolve(Field $field, array $response): string
     {
-        return PriceResolver::resolvePrice($field, $response, 'prices_min');
+        $currency = Str::of($field->valueKey)->after(' ')->toString();
+
+        $initial_price = self::getPrice($response, 'prices_min_initial', $currency);
+        $sale_price = self::getPrice($response, 'prices_min', $currency);
+
+        return $sale_price !== $initial_price ? "{$sale_price} {$currency}" : '';
+    }
+
+    private static function getPrice(array $response, string $field_name, string $currency): string
+    {
+        $prices = collect(Arr::get($response, $field_name));
+        return Arr::get(
+            $prices->firstWhere('currency', '=', $currency),
+            'gross',
+            '0.00',
+        );
     }
 }
--- tests/Feature/LocalResolvers/SalePriceResolverTest.php
+++ tests/Feature/LocalResolvers/SalePriceResolverTest.php
@@ -18,9 +18,45 @@ it('resolve field', function () {
     )->toEqual('10.00 PLN');
 });
 
+it('resolve field with price initial', function () {
+    expect(
+        SalePriceResolver::resolve(
+            mockField(new SalePriceResolver(), valueKey: 'sale_price PLN'),
+            [
+                'prices_min_initial' => [
+                    ['gross' => '14.00', 'currency' => 'EUR'],
+                    ['gross' => '40.00', 'currency' => 'PLN'],
+                ],
+                'prices_min' => [
+                    ['gross' => '4.00', 'currency' => 'EUR'],
+                    ['gross' => '10.00', 'currency' => 'PLN'],
+                ],
+            ],
+        ),
+    )->toEqual('10.00 PLN');
+});
+
+it('resolve field no sale', function () {
+    expect(
+        SalePriceResolver::resolve(
+            mockField(new SalePriceResolver(), valueKey: 'sale_price PLN'),
+            [
+                'prices_min_initial' => [
+                    ['gross' => '4.00', 'currency' => 'EUR'],
+                    ['gross' => '10.00', 'currency' => 'PLN'],
+                ],
+                'prices_min' => [
+                    ['gross' => '4.00', 'currency' => 'EUR'],
+                    ['gross' => '10.00', 'currency' => 'PLN'],
+                ],
+            ],
+        ),
+    )->toEqual('');
+});
+
 it('resolve field when there is no info', function () {
     expect(SalePriceResolver::resolve(
         mockField(new SalePriceResolver(), valueKey: 'sale_price PLN'),
         [],
-    ))->toEqual('0.00 PLN');
+    ))->toEqual('');
 });
